ae71fa46c350d815d0605ba5a2010d5b14469e8f
Sam <sfd9139@g.rit.edu>
2022-04-16 12:07:20 -0400

    step1 of password change function

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -22,6 +22,20 @@ const login = (req, res) => {
     return res.json({ redirect: '/maker' });
   });
 };
+const change = (req, res) => {
+  const username = `${req.body.username}`;
+  const pass = `${req.body.pass}`;
+  if (!username || !pass) {
+    return res.status(400).json({ error: 'Requires all fields.' });
+  }
+  return Account.authenticate(username, pass, (err, account) => {
+    if (err || !account) {
+      return res.status(401).json({ error: 'Wrong username or password.' });
+    }
+    req.session.account = Account.toAPI(account);
+    return res.json({ redirect: '/maker' });
+  });
+};
 const signup = async (req, res) => {
   const username = `${req.body.username}`;
   const pass = `${req.body.pass}`;
@@ -50,6 +64,7 @@ module.exports = {
   loginPage,
   login,
   logout,
+  change,
   signup,
   getToken,
 };
