d13de768d3959cc55c1b3ddb878ee127fe78a647
Sam <sfd9139@g.rit.edu>
2022-04-27 17:03:48 -0400

    tried to add statement that prevents dupes

--- client/maker.jsx
+++ client/maker.jsx
@@ -10,8 +10,27 @@ const handleAnime = (e) => {
         helper.handleError('All fields are required.');
         return false;
     }
+    checkForDupes(name);
     helper.sendPost(e.target.action, {name,genre,year,_csrf}, loadAnimesFromServer);
     return false;
+}
+const checkForDupes=(newName)=>{
+    const response = await fetch('/getAnimes');
+    const data = await response.json();
+    let animeArray=data.animes;
+    for (let list=0;list<animeArray.length;list++){
+        if(animeArray[list].name===newName){
+            helper.handleError();
+            return false;
+        }
+    }
+}
+const loadAnimesFromServer=async()=>{
+    const response = await fetch('/getAnimes');
+    const data = await response.json();
+    ReactDOM.render(
+        <AnimeList animes={data.animes} />, document.getElementById('animes')
+    );
 }
 const premInit = (e) => {
     e.preventDefault();
@@ -55,7 +74,7 @@ const AnimeForm=(props)=>{
         <form id="animeForm" onSubmit={handleAnime}
         name="animeForm" action="/maker"
         method="POST" className="animeForm">
-            <input id="animeName" type="text" name="name" placeholder="Anime Title" /><br></br><br></br>
+            <input className='animeField' id="animeName" type="text" name="name" placeholder="Anime Title" /><br></br><br></br>
             <label htmlFor="animeGenre">Genre: </label>
             <select className='animeField' id="animeGenre" name="genre">
                 <option className='animeField' value="Action">Action</option>
@@ -75,7 +94,7 @@ const PremiumForm=(props)=>{
         <form id="animeForm" onSubmit={handleAnime}
         name="animeForm" action="/maker"
         method="POST" className="animeForm">
-            <input id="animeName" type="text" name="name" placeholder="Anime Title" /><br></br><br></br>
+            <input className='animeField' id="animeName" type="text" name="name" placeholder="Anime Title" /><br></br><br></br>
             <label htmlFor="animeGenre">Genre: </label>
             <select className='animeField' id="animeGenre" name="genre">
                 <option className='animeField' value="Action">Action</option>
@@ -92,13 +111,6 @@ const PremiumForm=(props)=>{
             <input id="_csrf" type="hidden" name="_csrf" value={props.csrf} />
             <input className="makeAnimeSubmit" type="submit" value="Add to list" />
         </form>
-    );
-}
-const loadAnimesFromServer=async()=>{
-    const response = await fetch('/getAnimes');
-    const data = await response.json();
-    ReactDOM.render(
-        <AnimeList animes={data.animes} />, document.getElementById('animes')
     );
 }
 const init = async (prem) => {
--- server/controllers/Anime.js
+++ server/controllers/Anime.js
@@ -10,23 +10,23 @@ const getAnimes = (req, res) => AnimeModel.findByOwner(req.session.account._id, 
   }
   return res.json({ animes: docs });
 });
-const makeAnime = async (req, res) => {//adds the anime to the list; not done
+const makeAnime = async (req, res) => { // adds the anime to the list; not done
   if (!req.body.name || !req.body.genre || !req.body.year) {
     return res.status(400).json({ error: 'All fields are required.' });
   }
-  const animeData = {//the data of the anime that's being added
+  const animeData = { // the data of the anime that's being added
     name: req.body.name,
     genre: req.body.genre,
     year: req.body.year,
     owner: req.session.account._id,
   };
-  try {//attempts to add the anime to the list
+  try { // attempts to add the anime to the list
     const newAnime = new Anime(animeData);
     await newAnime.save();
     return res.status(201).json({ name: newAnime.name, genre: newAnime.genre, year: newAnime.year });
   } catch (err) {
     console.log(err);
-    if (err.code === 11000) {//should prevent duplicates; it currently doesn't
+    if (err.code === 11000) { // should prevent duplicates; it currently doesn't
       return res.status(400).json({ error: 'Anime already exists.' });
     }
     return res.status(400).json({ error: 'An error occurred.' });
